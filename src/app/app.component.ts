d7c58ea1c2daacc7f5d42ae23833fdae6ff6192a
Gilberto Alvarez Gamboa <gilbertoalvarez514@hotmail.com>
2024-04-10 19:15:27 -0700

    se crea estructura de los servicios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 import { PresentationComponent } from './presentation/presentation.component';
+import { ServicesComponent } from './services/services.component';
 import { WorksComponent } from './works/works.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, PresentationComponent, WorksComponent],
+  imports: [RouterOutlet, PresentationComponent, ServicesComponent, WorksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/models/services.model.ts
@@ -0,0 +1,5 @@
+export interface Services {
+    image: string,
+    name: string,
+    description: string
+}
