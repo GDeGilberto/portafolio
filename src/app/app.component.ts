abf7711cfaf8361b43b8c7bc3a425f1c400a4aef
Gilberto Alvarez Gamboa <gilbertoalvarez514@hotmail.com>
2024-04-17 19:30:04 -0700

    arquitectura vista de testimonios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { RouterOutlet } from '@angular/router';
 import { PresentationComponent } from './presentation/presentation.component';
 import { ServicesComponent } from './services/services.component';
 import { WorksComponent } from './works/works.component';
+import { TestimonyComponent } from './testimony/testimony.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, PresentationComponent, ServicesComponent, WorksComponent],
+  imports: [RouterOutlet, PresentationComponent, ServicesComponent, WorksComponent, TestimonyComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
